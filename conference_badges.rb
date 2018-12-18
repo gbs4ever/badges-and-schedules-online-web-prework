9ff348d834da2754e0397a34b4aefd9027799417
s greenbaum <gbs4ever2@gmail.com>
2018-12-18 00:19:49 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,10 +8,20 @@ end
 
 
 def assign_rooms(attendees)
+<<<<<<< HEAD
  attendees.each_with_index.map{|item,index|
  "Hello, #{item}! You'll be assigned to room #{index+1}!"}
 end
 def printer(attendees)
  batch_badge_creator(attendees).each {|printers| puts printers  }
  assign_rooms(attendees).each {|rooms| puts rooms }
+=======
+ attendees.each_with_index{|item,index|
+  room_assignments=  [item,index]}
+
+
+
+  #[ "Hello, #{item}! You'll be assigned to room#{index}!"]}
+  #room_assignments
+>>>>>>> d76afd3ae8169b7d95d88e072f5e9dca86c162b0
 end
